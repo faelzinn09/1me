36b5c584c05e6a10251a0aa7bd2d117f41d816ef
faelzinn09 <rafael.marques.santos09@escola.pr.gov.br>
2024-11-06 06:45:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function mostra reultado()
 {var numero 1=10
     var numero 2=15
-    var resultado=
+    var resultado=numero 1+ numero 2
+ document.get element by id(resultado).inner html=resultado
 }
